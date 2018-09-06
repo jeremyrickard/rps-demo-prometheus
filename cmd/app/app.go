c7586505660c0624f46a1d6fdd041b9cc48dad00
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-09-06 13:48:40 -0600

    Moving to use golang x/time/rate

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -8,16 +8,18 @@ import (
 	"strconv"
 	"time"
 
+	"golang.org/x/time/rate"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type metrics struct {
-	currentCount int
-	lastCount    int
-	rps          float64
-	lastTime     time.Time
-}
+// type metrics struct {
+// 	currentCount int
+// 	lastCount    int
+// 	rps          float64
+// 	lastTime     time.Time
+// }
 
 var (
 	requestDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
@@ -59,56 +61,32 @@ func main() {
 		log.Fatalf("bad value for rps limit: %s", rpsLimitStr)
 	}
 
-	ctr := &metrics{
-		lastTime: time.Now(),
-	}
+	// ctr := &metrics{
+	// 	lastTime: time.Now(),
+	// }
 	throttledHandler := throttler(
-		ctr,
+		//	ctr,
 		rpsLimit,
 		sleepSeconds,
 		http.FileServer(http.Dir("/app/content")),
 	)
 	http.Handle("/metrics", promhttp.Handler())
 	http.Handle("/", instrumentHandler(throttledHandler))
-	go rpsTelemetryCalculator(ctr)
+	//	go rpsTelemetryCalculator(ctr)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func throttler(
-	ctr *metrics,
+	//ctr *metrics,
 	limit float64,
 	sleepSeconds int,
 	handler http.Handler,
 ) http.Handler {
+	limiter := rate.NewLimiter(rate.Limit(limit), 10)
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			if ctr.rps > limit {
-				log.Printf("throttle")
-				randomSleep := time.Duration((float64(sleepSeconds*1000) + (ctr.rps / limit))) * time.Millisecond
-				log.Printf("random sleep: %v", randomSleep)
-				time.Sleep(randomSleep)
-			}
-			// lets make all the requests take at least 5 ms
-			time.Sleep(5 * time.Millisecond)
+			limiter.Wait(r.Context())
 			handler.ServeHTTP(w, r)
-			ctr.currentCount++
 		},
 	)
-}
-
-func rpsTelemetryCalculator(
-	ctr *metrics,
-) {
-	tickChan := time.NewTicker(time.Second * 30).C
-	var count int
-	var t time.Time
-	for {
-		t = <-tickChan
-		duration := t.Sub(ctr.lastTime)
-		count = ctr.currentCount
-		ctr.rps = float64(count-ctr.lastCount) / duration.Seconds()
-		ctr.lastTime = t
-		ctr.lastCount = count
-		log.Printf("RPS: %v", ctr.rps)
-	}
 }
