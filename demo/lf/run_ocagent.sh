7a149a396a74df9fe64d399fc77ad4ca40819d75
Reiley Yang <reyang@microsoft.com>
2018-09-19 11:43:02 -0700

    cleanup

--- demo/lf/run_ocagent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# download localforwarder: TODO
-
-cd /lf/
-ls -la ./Microsoft.LocalForwarder.ConsoleHost 
-
-chmod +x ./Microsoft.LocalForwarder.ConsoleHost
-./Microsoft.LocalForwarder.ConsoleHost noninteractive
